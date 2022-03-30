e4d776040dff85c17f5ae81775a66b4cf0651c8d
arshia <arshiabdol@yahoo.com>
2022-03-29 22:01:38 -0400

    added fade-in animation and styling changes for pharma main

--- src/components/Pharma/Pharma.js
+++ src/components/Pharma/Pharma.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import './pharma.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCircleInfo,faCapsules } from '@fortawesome/free-solid-svg-icons'
-
+import FadeIn from 'react-fade-in/lib/FadeIn';
 
 export default class Pharma extends Component {
     
@@ -22,7 +22,6 @@ export default class Pharma extends Component {
             behavior:"smooth",
             block:"start"
         })
-        ;
     }
     
     
@@ -30,13 +29,15 @@ export default class Pharma extends Component {
     return (
       <div className='pharma-main'>
           <div className='pharma-image-container' style={{backgroundImage: 'url(./images/pharma-back.jpg)'}}>
-            <div className='pharma-image-text'>
-                <h1>Suplements designed by professionals</h1>
-                <p>Checkout of products below, feel free to contant us for more information.</p>
-                
-                <span className="scrollTo" onClick={this.goToInfo}>Who are we <FontAwesomeIcon icon={faCircleInfo} /></span><br/>
-                <span className="scrollTo" onClick={this.goToProduct}>Checkout our suplements <FontAwesomeIcon icon={faCapsules} /></span>
-            </div>
+            <FadeIn delay={400}>
+                <div className='pharma-image-text'>
+                    <h1>Suplements designed by professionals</h1>
+                    <p>Checkout of products below, feel free to contant us for more information.</p>
+                    
+                    <span className="scrollTo" onClick={this.goToInfo}>Who are we <FontAwesomeIcon icon={faCircleInfo} /></span><br/>
+                    <span className="scrollTo" onClick={this.goToProduct}>Checkout our suplements <FontAwesomeIcon icon={faCapsules} /></span>
+                </div>
+            </FadeIn>
           </div>
           <div ref={ (ref) => this.RefInfo=ref } className='pharma-info'>
                 
