983f23d6e785eed4e1737f7cc2dffc5cea4d971b
arshia <arshiabdol@yahoo.com>
2022-04-04 00:40:41 -0400

    added main real estate components

--- src/components/realEstate/RealEstate/RealEstate.js
+++ src/components/realEstate/RealEstate/RealEstate.js
@@ -4,7 +4,12 @@ import './RealEstate.css'
 export default class RealEstate extends Component {
   render() {
     return (
-      <div className='realEstate-main'>RealState</div>
+      <div className='realEstate-image-container' style={{backgroundImage:"url(./images/realEstate-back.jpg)"}}>
+        <div className="realEstate-top-header-container">
+          <div className='realEstate-top-header'>Proudly a Canadian <img className='logo' alt='logo' src='./images/canadian-flag.png' /> Company
+              </div>
+        </div>
+      </div>
     )
   }
 }
