4f8adde78ebca1d66f79e62e85b57fd553cbd375
Arshia Kalkhorani <arshiabdol@yahoo.com>
2022-04-05 14:24:28 -0700

    added about to realEstate

--- src/components/realEstate/About/About.js
+++ src/components/realEstate/About/About.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import './About.css'
-
 function About() {
   return (
-    <div>About</div>
+    <div className='about-card'>
+      <div className="realEstate-about-card-section">
+        <h2 className='realEstate-about-card-setion-headers'>Our Mission</h2>
+          <p>
+            Our mission is to find life find life changing supplements and bring them to you, Anywhere inside Canada working in tendom with Canadian businesses supporting them in the way.  
+          </p>
+      </div>
+      <div className="realEstate-about-card-section">
+        <h2 className='realEstate-about-card-setion-headers'>Our Past</h2>
+          <p>
+            Paradie is a company based in Ontario, Our CEO Javad Kalkhorani is a Pharmacist with 40 years of international experience working with companies like Merc and Aventis. So don't worry you are in good hands. 
+          </p>
+      </div>
+      <div className="realEstate-about-card-section">
+        <h2 className='realEstate-about-card-setion-headers'>What we do</h2>
+          <p>
+            Our business is based in supplements and real estate, we invest in new and proven products bringing them the attention they deserve. So if you think you fit the bill contact us were very excited to work with you.
+          </p>
+      </div>
+    </div>
   )
 }
 
