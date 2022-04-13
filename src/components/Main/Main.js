3dd04e81d2f3e6b2ed7cab4225eb8d51a5550938
arshia <arshiabdol@yahoo.com>
2022-04-12 21:30:04 -0400

    fixing main file name

---
+++ src/components/Main/Main.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import './main.css'
+
+
+
+
+export default class Main extends Component {
+  render() {
+    return (
+      <div className="main">
+        <div className="real-estate"> 
+          <Link to='RealEstate' className='real-estate-link'>  
+            <div className="real-estate-text">
+              <h1>Real Estate</h1>   
+            </div>     
+          </Link>
+          <img src='./images/main-real-estate-back.jpg' alt ="pharma"  />
+        </div>
+        
+        <div className="pharma">
+          <Link to='Pharma' className='pharma-link'> 
+            <div className="pharma-text">
+                <h1>Suplements</h1>
+            </div>
+          </Link>
+          <img src='./images/main-pharma-back.jpg' alt ="real-estate"  />
+        </div>
+      </div>
+    )
+  }
+}
